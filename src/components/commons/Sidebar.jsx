b81e4b01a35ac336cb07572b64e49bd511a75853
Ashutosh Padhi <ashutoshpadhi664@gmail.com>
2025-05-13 06:00:26 +0530

    Added active sidebar component UI

--- src/components/commons/Sidebar.jsx
+++ src/components/commons/Sidebar.jsx
@@ -1,48 +1,41 @@
 import React from "react";
 
 import { Favorite, List, TimeTracking, NeetoKb, Globe } from "neetoicons";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
-const Sidebar = () => (
-  <nav className="flex h-screen w-16 flex-col items-center space-y-6 border-r-2 border-gray-200 py-4">
-    <div className="neeto-ui-bg-black neeto-ui-text-white rounded-md p-0.5">
-      <Link to="/kanban">
-        <NeetoKb className="cursor-pointer" size={24} />
-      </Link>
-    </div>
-    <div>
-      <Link to="/kanban">
-        <List
-          className="hover:neeto-ui-text-white cursor-pointer rounded-md p-0.5 hover:bg-gray-900"
-          size={24}
-        />
-      </Link>
-    </div>
-    <div>
-      <Link to="/pomodoro">
-        <TimeTracking
-          className="hover:neeto-ui-text-white cursor-pointer rounded-md p-0.5 hover:bg-gray-900"
-          size={24}
-        />
-      </Link>
-    </div>
-    <div>
-      <Link to="/news">
-        <Globe
-          className="hover:neeto-ui-text-white cursor-pointer rounded-md p-0.5 hover:bg-gray-900"
-          size={24}
-        />
-      </Link>
-    </div>
-    <div>
-      <Link to="/favorite">
-        <Favorite
-          className="hover:neeto-ui-text-white cursor-pointer rounded-md p-0.5 hover:bg-gray-900"
-          size={24}
-        />
-      </Link>
-    </div>
-  </nav>
-);
+const Sidebar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const navLinks = [
+    { to: "/kanban", icon: List },
+    { to: "/pomodoro", icon: TimeTracking },
+    { to: "/news", icon: Globe },
+    { to: "/favorite", icon: Favorite },
+  ];
+
+  return (
+    <nav className="flex h-screen w-16 flex-col items-center space-y-6 border-r-2 border-gray-200 py-4">
+      <div className="neeto-ui-bg-black neeto-ui-text-white rounded-md p-0.5">
+        <Link to="/kanban">
+          <NeetoKb className="cursor-pointer" size={24} />
+        </Link>
+      </div>
+      {navLinks.map(({ to, icon: Icon }) => (
+        <div key={to}>
+          <Link to={to}>
+            <Icon
+              size={24}
+              className={`cursor-pointer rounded-md p-0.5 ${
+                currentPath === to
+                  ? "bg-black text-white"
+                  : "text-gray-800 hover:bg-gray-900 hover:text-white"
+              }`}
+            />
+          </Link>
+        </div>
+      ))}
+    </nav>
+  );
+};
 
 export default Sidebar;
