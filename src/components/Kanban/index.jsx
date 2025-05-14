c5fd8d277c87966494c69cb60bce71ca32fa2c40
Ashutosh Padhi <ashutoshpadhi664@gmail.com>
2025-05-14 23:24:49 +0530

    Fixed kanban mode ui

--- src/components/Kanban/index.jsx
+++ src/components/Kanban/index.jsx
@@ -19,7 +19,10 @@ const Kanban = () => {
       <h1 className="mb-12 ml-12 mt-6 self-start text-4xl font-bold">
         Kanban mode
       </h1>
-      <div className="flex h-96 w-full flex-row justify-between gap-12 px-12">
+      <div
+        className="flex w-full flex-row justify-between gap-12 px-12"
+        style={{ minHeight: "16rem" }}
+      >
         {KANBAN_COLUMN_IDS.map((columnId) => (
           <Column
             activeInputColumn={activeInputColumn}
