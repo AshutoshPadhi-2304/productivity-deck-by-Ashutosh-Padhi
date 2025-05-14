3c1cad2a7bdde3cc75659e3857ce9a5e366e8208
Ashutosh Padhi <ashutoshpadhi664@gmail.com>
2025-05-14 17:13:47 +0530

    Changed drag and drag functionality

--- src/components/Kanban/Column.jsx
+++ src/components/Kanban/Column.jsx
@@ -40,11 +40,10 @@ const Column = ({
 
   return (
     <div className="flex w-1/3 flex-col items-center justify-center rounded-lg border border-gray-900 p-4">
-      <h2 className="mb-4 ml-4 self-start text-xl font-bold">{label}</h2>
-      <DropArea onDrop={() => onDrop(columnId)} />
-      <div className="flex h-full w-full flex-col items-center">
-        {tasks.map((task) => (
-          <>
+      <DropArea onDrop={() => onDrop(columnId)}>
+        <h2 className="mb-4 ml-4 self-start text-xl font-bold">{label}</h2>
+        <div className="flex h-full w-full flex-col items-center">
+          {tasks.map((task) => (
             <div
               draggable
               className="active:opacity-0.8 group mb-2 flex w-full items-center rounded-md border border-gray-800 p-1"
@@ -82,22 +81,21 @@ const Column = ({
                 }}
               />
             </div>
-            <DropArea onDrop={() => onDrop(columnId)} />
-          </>
-        ))}
-        {activeInputColumn === columnId && (
-          <div className="mb-2 flex w-full items-center rounded-md p-1">
-            <Input
-              autoFocus
-              placeholder="Enter task"
-              type="text"
-              value={newTask}
-              onChange={(e) => setNewTask(e.target.value)}
-              onKeyDown={(e) => e.key === "Enter" && handleAddNewTask()}
-            />
-          </div>
-        )}
-      </div>
+          ))}
+          {activeInputColumn === columnId && (
+            <div className="mb-2 flex w-full items-center rounded-md p-1">
+              <Input
+                autoFocus
+                placeholder="Enter task"
+                type="text"
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && handleAddNewTask()}
+              />
+            </div>
+          )}
+        </div>
+      </DropArea>
       <Button
         className="bg-transparent text-black hover:bg-gray-400 "
         label="Add new task +"
--- src/components/Kanban/DropArea.jsx
+++ src/components/Kanban/DropArea.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 
-const DropArea = ({ onDrop }) => {
+const DropArea = ({ onDrop, children }) => {
   const [showDrop, setShowDrop] = useState(false);
 
   return (
     <div
-      className={`mb-2 w-full rounded-md border border-dashed border-gray-800 bg-gray-900 p-2 ${
-        showDrop ? "opacity-100" : "opacity-0"
-      }`}
+      className="} mb-2 h-full w-full rounded-md bg-transparent"
       onDragEnter={() => setShowDrop(true)}
       onDragLeave={() => setShowDrop(false)}
       onDragOver={(e) => e.preventDefault()}
       onDrop={() => {
         onDrop();
         setShowDrop(false);
+        console.log(showDrop);
       }}
     >
-      <hr />
+      {children}
     </div>
   );
 };
